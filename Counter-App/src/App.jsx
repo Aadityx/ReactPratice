59eccb3925f45fe6acb8b911402ce999fc706327
Aadityx <aadityapatil784@gmail.com>
2026-03-31 09:33:57 +0530

    buttons added

--- Counter-App/src/App.jsx
+++ Counter-App/src/App.jsx
@@ -5,14 +5,26 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const a = "Counter App"
+  const [count, setCount] = useState(0);
+  const incr = () =>{
+    setCount(count+1);
+  }
+  const decr = () => {
+    setCount(count-1);
+  }
+  const reset = () => {
+    setCount(0);
+  }
   return (
     <div>
-      <h1>Counter App</h1>
-      <h2>ReactJS based counter app</h2>
+      <h1>{a}</h1>
+      <h2>Current Value = {count}</h2>
+      <button onClick={incr}>Increment</button>
+      <button onClick={decr}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 }
 
-export default App
+export default App 
