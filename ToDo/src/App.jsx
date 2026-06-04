b7ac954d72d620f7d522a815333c8f41257ef18b
Aadityx <aadityapatil784@gmail.com>
2026-06-04 12:11:38 +0530

    Added todo list

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css';
-import { Container, Form, AddButton } from 'react-bootstrap';
+import'./App.css';
+import { Container } from 'react-bootstrap';
+import ToDoForm from './ToDoForm';
+import ToDoList from './ToDoList';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (text) => {
+    const todo = { id: Date.now(), text: text, complete: false };
+    setTodos([...todos, todo]);
+  };
+
+  function deleteTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+  
   return (
     <Container className="App">
       <h1>ToDo App</h1>
-      <ToDoForm />
+      <ToDoForm onAddTodo={addTodo} />
+      <ToDoList todos={todos} onDeleteTodo={deleteTodo} />
     </Container>
   );
 }
