291f4f74d3e555cb8e4562d5c2e6795fa867c7d1
Aadityx <aadityapatil784@gmail.com>
2026-06-04 12:27:29 +0530

    Completed ToDo

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,5 +1,5 @@
 import'./App.css';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import ToDoForm from './ToDoForm';
 import ToDoList from './ToDoList';
 import { useState } from 'react';
@@ -15,11 +15,26 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id));
   };
   
+  function markComplete(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, complete: !todo.complete };
+      }
+      return todo;
+    }));
+  };
+
+  function deleteCompleted() {
+    setTodos(todos.filter(todo => !todo.complete));
+  };
   return (
     <Container className="App">
       <h1>ToDo App</h1>
       <ToDoForm onAddTodo={addTodo} />
-      <ToDoList todos={todos} onDeleteTodo={deleteTodo} />
+      <ToDoList todos={todos} onDeleteTodo={deleteTodo} markComplete={markComplete} />
+      <Button variant="secondary" onClick={() => deleteCompleted()}>
+        Clear completed tasks
+      </Button>
     </Container>
   );
 }
--- ToDo/src/ToDoList.jsx
+++ ToDo/src/ToDoList.jsx
@@ -1,6 +1,6 @@
 import { Card } from "react-bootstrap";
 
-function ToDoList({ todos }) {
+function ToDoList({ todos, onDeleteTodo, markComplete }) {
     return (
         <div>
             <h2>ToDo List</h2>
@@ -8,8 +8,14 @@ function ToDoList({ todos }) {
                 {todos.map((todo) => (
                     <Card style={{ width: '18rem' }}>
                         <Card.Body>
+                            <button variant="primary" onClick={() => markComplete(todo.id)}>
+                                Change Status
+                            </button>
+                            Completed {todo.complete ? 'Yes' : 'No'}
                             <Card.Title>{todo.text}</Card.Title>
-                            <button variant="danger">Delete</button>
+                            <button variant="danger" onClick={() => onDeleteTodo(todo.id)}>
+                                Delete
+                            </button>
                         </Card.Body>
                     </Card>
                 ))}
