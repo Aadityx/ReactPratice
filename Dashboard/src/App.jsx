fe26e79f5c1c40f66737b84f84fb5e7d69e101ab
Aadityx <aadityapatil784@gmail.com>
2026-06-06 17:23:45 +0530

    added routing path

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -1,34 +1,35 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowerserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
 import Home from './components/home';
 import Contact from './components/Contact';
 import Login from './components/Login';
 import SignUp from './components/Signup';
 import Dashboard from './components/Dashboard';
+import {Nav} from 'react-bootstrap';
 
 function App() {
 
   return (
     <div>
+      <h1>Welcome to the Dashboard</h1>
       <BrowserRouter>
-      <Routes>
-        <Route path ="/" element={<Home />} />
-        <Route path ="/about" element={<Contact />} />
-        <Route path ="/contact" element={<Login />} />
-        <Route path ="/signup" element={<SignUp />} />
-        <Route path ="/dashboard" element={<Dashboard />} />
-      </Routes>
-      </BrowserRouter>
-
-
       <Nav className="navbar">
         <NavLink className="nav-link" to="/">Home</NavLink>
-        <NavLink className="nav-link" to="/about">About</NavLink>
+        <NavLink className="nav-link" to="/login">Login</NavLink>
         <NavLink className="nav-link" to="/contact">Contact</NavLink>
         <NavLink className="nav-link" to="/signup">Signup</NavLink>
         <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
       </Nav>
+
+      <Routes>
+        <Route path ="/" element={<Home />} />
+        <Route path ="/contact" element={<Contact />} />
+        <Route path ="/login" element={<Login />} />
+        <Route path ="/signup" element={<SignUp />} />
+        <Route path ="/dashboard" element={<Dashboard />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
