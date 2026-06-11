3913157464d98f77e1e098879bbd6649dc23cf02
Aadityx <aadityapatil784@gmail.com>
2026-06-11 12:21:32 +0530

    added jwt tokens

--- POSTMAN/app.js
+++ POSTMAN/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connectDB = require('./db');
 const User = require('./models/users');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'your_secret_key';
 
 const app = express();
 connectDB();
@@ -10,7 +12,7 @@ app.use(express.json());
 const PORT = 5000;
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK' });
+    res.status(200).json({ status: 'OK' });
 });
 
 app.post('/register', async (req, res) => {
@@ -32,19 +34,46 @@ app.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({ message: 'Invalid credentials' });
+            return res.status(400).json(
+                {
+                    status: 'Bad Request',
+                    message: 'Invalid credentials'
+                }
+            );
         }
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid credentials' });
+            return res.status(400).json(
+                {
+                    status: 'Bad Request',
+                    message: 'Invalid credentials'
+                }
+            );
         }
-        res.status(200).json({ message: 'Login successful' });
+
+        const token = jwt.sign(
+            {
+                email: user.email,
+                name: user.name
+            }, SECRET_KEY, { expiresIn: '1h' });
+
+        res.status(200).json(
+            {
+                status: 'OK',
+                message: 'Login successful',
+                token: token
+            });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json(
+            {
+                status: 'Internal Server Error',
+                message: 'Server error',
+            }
+        );
     }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
